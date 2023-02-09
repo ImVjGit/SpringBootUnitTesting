3ba676122810ad129d85bc7dd444fd57a872a15b
ImVjGit <imvjgit@gmail.com>
2023-02-09 09:23:03 +0530

    Own Unit Testing

---
+++ SpringBootUnitTesting/SBMCD1/src/main/SBMCD1/Sbmcd1Application.java
@@ -0,0 +1,16 @@
+package com.SBMCD1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+
+public class Sbmcd1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbmcd1Application.class, args);
+	}
+
+}
---
+++ SpringBootUnitTesting/SBMCD1/src/test/java/com/SBMCD1/Sbmcd1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SBMCD1;
+
+
+
+
+@SpringBootTest
+class Sbmcd1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
